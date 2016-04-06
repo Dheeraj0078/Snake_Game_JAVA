9fd59617b6fb6c3040bcb072722d4bc70cef9cb8
Mateusz <mateuszbucko1@gmail.com>
2016-04-06 18:51:03 +0200

    forget about 1 class

---
+++ Snake_Game/src/mateusz/bucko/snake/Main.java
@@ -0,0 +1,11 @@
+package mateusz.bucko.snake;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Menu_Window gui = new Menu_Window();
+		Printer print = new Printer();
+
+	}
+
+}
